2f82f5f61bc62c1c930e3cd69256b8ddcadf4964
Mirova Natacha Salas Rios <msalas@baufest.com>
2020-04-27 20:25:15 -0300

    cambio en el branch nuevo

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	public static void test () {
+		System.out.println("prueba");
 	}
 
 }
